5bbafb73498db1039137aac90dd4b299f6876e69
Jozef Kysel <jozef.kysel@qest.cz>
2020-07-15 17:16:00 +0200

    test(user-service): Add unit tests for user service

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-Help `python3 ugen.py --help`
+Help 
+```
+cd app
+python3 ugen.py --help
+```
 
 Run
 ```
 cd app
 python3 ugen.py –-output [output file] [input file]...
+```
+
+Run unit tests
+```
+cd app
+python3 -m unittest
 ```
---
+++ app/tests/test_user_service.py
@@ -0,0 +1,67 @@
+from user_service import generateUsername, createUserRecord
+import unittest
+
+class TestUserService(unittest.TestCase):
+  
+  id = '1234'
+  givenNames = ['John', 'Stuart']
+  lastName = 'Doe'
+  department = 'Research'
+  username = 'jsdoe'
+  colon = ':'
+  
+  def test_generateUsername(self):
+        """
+        Test that it can generate username
+        """
+        result = generateUsername(self.givenNames, self.lastName)
+        self.assertEqual(result, self.username)
+  
+  def test_generateUsername_no_middle_names(self):
+        """
+        Test that it not fail when user has not middle name
+        """
+        givenNames = ['John']
+        result = generateUsername(givenNames, self.lastName)
+        self.assertEqual(result, 'jdoe')
+  
+  def test_generateUsername_no_given_names(self):
+        """
+        Test that it not fail when user has not givenNames
+        """
+        givenNames = []
+        result = generateUsername(givenNames, self.lastName)
+        self.assertEqual(result, 'doe')
+  
+  def test_createUserRecord(self):
+        """
+        Test that it will create user record
+        """
+        result = createUserRecord(self.id, self.givenNames, self.lastName, self.department)
+        expectedResult = self.colon.join([self.id, self.username, self.colon.join(self.givenNames), self.lastName, self.department])
+        self.assertEqual(result, expectedResult)
+  
+  def test_createUserRecord_no_given_names(self):
+        """
+        Test that it will not fail when user has not givenNames
+        """
+        givenNames = []
+        username = 'doe'
+        result = createUserRecord(self.id, givenNames, self.lastName, self.department)
+        expectedResult = self.colon.join([self.id, username, self.colon.join(givenNames), self.lastName, self.department])
+        self.assertEqual(result, expectedResult)
+  
+  def test_createUserRecord_no_middle_name(self):
+        """
+        Test that it will not fail when user has not middle name
+        """
+        givenNames = ['John']
+        username = 'jdoe'
+        result = createUserRecord(self.id, givenNames, self.lastName, self.department)
+        expectedResult = self.colon.join([self.id, username, self.colon.join(givenNames), self.lastName, self.department])
+        self.assertEqual(result, expectedResult)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
